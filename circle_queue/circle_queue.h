d34cdcb09b7cdf34017bfcbc357c0810240b075c
Jason416 <1689229721@qq.com>
2017-08-19 14:19:08 +0800

    circle_queue ADT

---
+++ circle_queue/circle_queue.h
@@ -0,0 +1,22 @@
+#ifndef __CIRCLE_QUEUE_H
+#define __CIRCLE_QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Base.h"
+
+typedef int ELemType;
+typedef struct node
+{
+
+
+};
+
+
+
+
+
+
+
+#endif
---
+++ circle_queue/test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    short s = 4 ;
+    s = s + 5;
+    printf("%d\n",s);
+    return 0;
+
+
+}
